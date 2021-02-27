94b5b75b4841ffa5f786610141a77b4d32a25c76
vonot <rafaelfeliperm@gmail.com>
2021-02-26 23:05:00 -0300

    Mudança de Projeto | Começo de Implementação

--- server.R
+++ server.R
@@ -1,8 +1,13 @@
 #server.R
 
-library(shiny)
-
+data <- read.csv(file = 'data.csv')
 server <- function(input, output, session) {
-  
+  observe({
+    language<- input$languages
+    info <- subset(data, name==language)
+    pop <- info[1,3]
+    image <- info[1,4]
+    js$changeLogo(image)
+  })
+                  
 }
-
--- ui.R
+++ ui.R
@@ -2,10 +2,60 @@
 
 library(shiny)
 library(shinydashboard)
+library(shinyjs)
+
+data <- read.csv(file = 'data.csv')
+
+for(i in data){
+  choices<- data[i,2]
+}
+
+jsCode <- "
+shinyjs.changeLogo = function(image){
+  var logo = document.getElementById('logo')
+  logo.src = image
+}
+"
 
 ui <- dashboardPage(
-  dashboardHeader(title = "Movie Dashboard"),
-  dashboardSidebar(),
-  dashboardBody()
+  dashboardHeader(title = "Dash LP"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Dashboard", tabName = "dash", icon = icon("dashboard")),
+      menuItem("Compare", tabName = "comp")
+    )
+  ),
+  dashboardBody(
+    useShinyjs(),
+    extendShinyjs(text = jsCode, functions = c("changeLogo")),
+    tabItems(
+      tabItem(tabName = "dash",
+              fluidRow(
+                column(12,
+                       selectInput("languages", "Select Languges", choices = choices, multiple = FALSE, selected = NULL, width = "100%"), 
+                )
+              ),
+              fluidRow(
+                column(6,
+                       img(id = "logo", height = "200px", width = "200px")),
+                column(6,
+                       fluidRow(
+                         column(12,
+                                p("Hello"))
+                       ),
+                       fluidRow(
+                         column(6,
+                                p("Create By: ")),
+                         column(6,
+                                p("At:"))
+                       )
+                )
+              )
+      ),
+      
+      tabItem( tabName = "comp",
+        p("OLa")
+      )
+    )
 )
-
+)
