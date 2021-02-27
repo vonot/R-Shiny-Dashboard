8d897d42f45047b0777b5cac29c59f1aa8bc2427
vonot <rafaelfeliperm@gmail.com>
2021-02-27 01:54:39 -0300

    Implementado os primeiros graficos

--- server.R
+++ server.R
@@ -1,13 +1,40 @@
 #server.R
+library(shinyjs)
+library(dplyr)
 
 data <- read.csv(file = 'data.csv')
 server <- function(input, output, session) {
   observe({
     language<- input$languages
     info <- subset(data, name==language)
-    pop <- info[1,3]
-    image <- info[1,4]
-    js$changeLogo(image)
+    image <- info[1,3]
+    author <- info[1,4]
+    release <- info[1,5]
+    version <- info[1,6]
+    popyear20 <- info[1,7]
+    popyear19 <- info[1,8]
+    popyear18 <- info[1,9]
+    popyear17 <- info[1,10]
+    loved <- info[1,11]
+    dread <- info[1,12]
+    wanted <- info[1,13]
+    
+    js$loadData(image, author, release, version)
+    
+    output$plot <- renderPlot({
+      range <- input$slider[1]:input$slider[2]
+      x <- range
+      y <- c(popyear17, popyear18, popyear19,popyear20)
+      
+      plot(x,y, type = "l")
+    })
+    
+    output$plot2 <- renderPlot({
+      value <- c(loved, dread, wanted)
+      group <- c("Gostam", "Odeiam", "Querem")
+      
+      pie(value, labels = group, radius = 0.8)
+    })
   })
                   
 }
--- ui.R
+++ ui.R
@@ -5,15 +5,29 @@ library(shinydashboard)
 library(shinyjs)
 
 data <- read.csv(file = 'data.csv')
-
-for(i in data){
-  choices<- data[i,2]
-}
+print(data)
+choices <- data[2]
+print(choices)
 
 jsCode <- "
-shinyjs.changeLogo = function(image){
+shinyjs.loadData = function(params){
+  var defaultParams = {
+    image : null,
+    author : null,
+    release : 2021,
+    version : 1
+  };
+  
+  params = shinyjs.getParams(params, defaultParams);
+  
   var logo = document.getElementById('logo')
-  logo.src = image
+  var author = document.getElementById('author')
+  var release = document.getElementById('year')
+  var version = document.getElementById('version')
+  logo.src = params.image
+  author.innerHTML = params.author
+  release.innerHTML = params.release
+  version.innerHTML = params.version
 }
 "
 
@@ -22,12 +36,14 @@ ui <- dashboardPage(
   dashboardSidebar(
     sidebarMenu(
       menuItem("Dashboard", tabName = "dash", icon = icon("dashboard")),
-      menuItem("Compare", tabName = "comp")
+      menuItem("Compare",
+                  menuSubItem("Lista das Mais Populares 2020", tabName = "lista"),
+                  menuSubItem("Compare Linguagens", tabName = "compare"))
     )
   ),
   dashboardBody(
     useShinyjs(),
-    extendShinyjs(text = jsCode, functions = c("changeLogo")),
+    extendShinyjs(text = jsCode, functions = c("loadData")),
     tabItems(
       tabItem(tabName = "dash",
               fluidRow(
@@ -39,22 +55,30 @@ ui <- dashboardPage(
                 column(6,
                        img(id = "logo", height = "200px", width = "200px")),
                 column(6,
-                       fluidRow(
-                         column(12,
-                                p("Hello"))
-                       ),
-                       fluidRow(
-                         column(6,
-                                p("Create By: ")),
-                         column(6,
-                                p("At:"))
-                       )
+                       p(id="year"),
+                       p(id="author"),
+                       p(id="version"),
+                )
+              ),
+              fluidRow(
+                column(6,
+                       plotOutput("plot"),
+                       sliderInput(max = 2020, min = 2017, inputId = "slider", label = "Select Range of Plot", value = c(2017,2020))
+                       
+                ),
+                column(6,
+                       plotOutput("plot2")
                 )
               )
+              
       ),
       
-      tabItem( tabName = "comp",
+      tabItem( tabName = "lista",
         p("OLa")
+      ),
+      
+      tabItem(tabName = "compare",
+      p("hello")
       )
     )
 )
