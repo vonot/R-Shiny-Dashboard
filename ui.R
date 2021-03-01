0c7df98c233e2db6eb9230d39b361477e57a82dc
vonot <rafaelfeliperm@gmail.com>
2021-03-01 01:09:38 -0300

    Tabela de Popularidade Atualizada

--- ui.R
+++ ui.R
@@ -46,34 +46,66 @@ shinyjs.tableData = function(params){
     
     //Inicialização das tabelas
     
-    var table20 = document.getElementById('table20')
-    var table20 = document.getElementById('table19')
-    var table20 = document.getElementById('table18')
-    var table20 = document.getElementById('table17')
+    let tabelas = {'2020' : document.getElementById('table20'),
+                   '2019' : document.getElementById('table19'),
+                   '2018' : document.getElementById('table18'),
+                   '2017' : document.getElementById('table17')}
     
     //Montagem das tabelas
     
-    //2020
-    
-    table20.innerHTLM += '<tbody>'
-    for(let i = 0; i<length; i++){
-      if(dados[i].x2020 != null){}
-      
+    const construirTabela = (year) =>{
+        let place = 1
+        
+        dados.sort(function (a, b) {
+	
+	        return (a['x' + year] < b['x' + year]) ? 1 : ((b['x' + year] < a['x' + year]) ? -1 : 0);
+ 
+        });
+        
+        for(let i = 0; i<length; i++){
+          if(dados[i]['x' + year]!=null){
+            let row = document.createElement('tr')
+            let colPlace = document.createElement('td')
+            let colLanguage = document.createElement('td')
+            let colBar = document.createElement('td')
+            
+            let infoPlace = document.createTextNode(place)
+            let infoLanguage = document.createTextNode(dados[i].nome)
+            let infoBar = document.createTextNode(dados[i]['x' + year] +'%')
+            
+            tabelas[year].appendChild(row)
+            row.appendChild(colBar)
+            row.insertBefore(colLanguage, colBar)
+            row.insertBefore(colPlace, colLanguage)
+        
+            colPlace.appendChild(infoPlace)
+            colLanguage.appendChild(infoLanguage)
+            colBar.appendChild(infoBar)
+            
+            place++
+          }
+      }
     }
     
+    construirTabela('2020')
     
+    construirTabela('2019')
+  
+    construirTabela('2018')
+    
+    construirTabela('2017')
 }  
 "
 
-ui <- dashboardPage(
+ui <-dashboardPage(
   
   dashboardHeader(title = "Dash LP"),
   dashboardSidebar(
     sidebarMenu(
       menuItem("Dashboard", tabName = "dash", icon = icon("dashboard")),
       menuItem("Compare",
-                  menuSubItem("Lista das Mais Populares 2020", tabName = "lista"),
-                  menuSubItem("Compare a Popularidade", tabName = "compare"))
+               menuSubItem("Lista das Mais Populares 2020", tabName = "lista"),
+               menuSubItem("Compare a Popularidade", tabName = "compare"))
     )
   ),
   dashboardBody(
@@ -83,24 +115,24 @@ ui <- dashboardPage(
       tabItem(tabName = "dash",
               fluidRow(
                 column(12,
-                       selectInput("languages", "Selecione a Linguagem", choices = choices, multiple = FALSE, selected = NULL, width = "100%"), 
+                       selectInput("languages", "Selecione a Linguagem", choices = choices, multiple = FALSE, selected = NULL, width = "100%"),
                 ),
                 column(6,
                        box(
                          title = "Informação sobre a Linguagem", status = "primary", solidHeader = TRUE, collapsible = FALSE, width = 12,
                          column(4,
-                                img(id = "logo", height = "100%", width = "100%", style="border-radius:10px;")
+                                img(id = "logo", height = "100%", width = "100%", style = "border-radius:10px;")
                          ),
                          column(4,
                                 box(
-                                  title = "Historia", solidHeader = TRUE, width = 12,
+                                  title = "Historia", solidHeader = TRUE, width = 12, collapsible = TRUE,
                                   p("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tristique justo quis leo volutpat, id vestibulum risus tempus. Morbi fringilla tempus risus sit amet accumsan.")
                                 )
-                          ),
+                         ),
                          column(4,
                                 valueBoxOutput("author", width = 12),
                                 valueBoxOutput("release", width = 12),
-                                )
+                         )
                          
                        )
                 ),
@@ -127,64 +159,74 @@ ui <- dashboardPage(
                 )
               )
       ),
-      tabItem( tabName = "lista",
-               fluidRow(
-                 tabBox(
-                 title = "Lista das Linguagens mais populares",
-                 id = "table", height = "250px",
-                 tabPanel("2020", 
-                          tags$table(class= "table", id="table20",
-                                     tags$thead( class="thead-light",
-                                                 tags$tr(
-                                                   tags$th("#"),
-                                                   tags$th("Linguagem"),
-                                                   tags$th("Popularidade"),
-                                                 )
-                                       
-                                     ))
-                            ), 
-                 tabPanel("2019",
-                          tags$table(class= "table", id="table19",
-                                     tags$thead( class="thead-light",
-                                                 tags$tr(
-                                                   tags$th("#"),
-                                                   tags$th("Linguagem"),
-                                                   tags$th("Popularidade"),
-                                                 )
-                                                 
-                                     ))
-                          ),
-                 tabPanel("2018", 
-                          tags$table(class= "table", id="table18",
-                                     tags$thead( class="thead-light",
-                                                 tags$tr(
-                                                   tags$th("#"),
-                                                   tags$th("Linguagem"),
-                                                   tags$th("Popularidade"),
-                                                 )
-                                                 
-                                     ))
-                          ),
-                 tabPanel("2017",
-                          tags$table(class= "table", id="table17",
-                                     tags$thead( class="thead-light",
-                                                 tags$tr(
-                                                   tags$th("#"),
-                                                   tags$th("Linguagem"),
-                                                   tags$th("Popularidade"),
-                                                 )
-                                                 
-                                     ))
-                          )
+      tabItem(tabName = "lista",
+              fluidRow(
+                tabBox(
+                  title = "Lista das Linguagens mais populares",
+                  id = "table", height = "250px",
+                  tabPanel("2020",
+                           tags$table(class = "table",
+                                      tags$thead(
+                                        tags$tr(
+                                          tags$th("#"),
+                                          tags$th("Linguagem"),
+                                          tags$th("Popularidade")
+                                        )
+                                      ),
+                                      
+                                      tags$tbody(id = "table20")
+                                      
+                           )
+                  ),
+                  tabPanel("2019",
+                           tags$table(class = "table",
+                                      tags$thead(
+                                        tags$tr(
+                                          tags$th("#"),
+                                          tags$th("Linguagem"),
+                                          tags$th("Popularidade")
+                                        )
+                                      ),
+                                      
+                                      tags$tbody(id = "table19")
+                                      
+                           )
+                  ),
+                  tabPanel("2018",
+                           tags$table(class = "table",
+                                      tags$thead(
+                                        tags$tr(
+                                          tags$th("#"),
+                                          tags$th("Linguagem"),
+                                          tags$th("Popularidade")
+                                        )
+                                      ),
+                                      
+                                      tags$tbody(id = "table18")
+                                      
+                           )
+                  ),
+                  tabPanel("2017",
+                           tags$table(class = "table",
+                                      tags$thead(
+                                        tags$tr(
+                                          tags$th("#"),
+                                          tags$th("Linguagem"),
+                                          tags$th("Popularidade")
+                                        )
+                                      ),
+                                      
+                                      tags$tbody(id = "table17")
+                                      
+                           )
+                  )
                 )
-               )
       ),
       
       tabItem(tabName = "compare",
-      
-      taskItem(value = 90, color = "green",
-               "Teste")
+              
+              taskItem(value = 90, color = "green",
+                       "Teste")
       )
     )
-  )
-)
+  ))
